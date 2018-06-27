c52f9c5e02d2d215d95acfb157114af7beb25a88
harleyjj <harleyyesm@gmail.com>
2018-06-27 11:07:07 -0700

    added Input.js

--- src/components/delivery-form.js
+++ src/components/delivery-form.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import {reduxForm, Field, focus} from 'redux-form';
+import Input from './input';
 
 export class DeliveryForm extends React.Component {
     render() {
         return (
         <form>
             <h2>Report a problem with your delivery</h2>
-            <label htmlFor="tracking">Tracking number</label>
-            <Field name="tracking" type="text" component="input" label="Tracking number"/>
-            <label htmlFor="issue">What is your issue?</label>
-            <Field name="issue" type="issue" component={() => {
-                return(
-                    <select>
-                        <option value="late">My delivery hasn't arrived</option>
-                        <option value="wrong">The wrong item was delivered</option>
-                        <option value="missing">Part of my order was missing</option>
-                        <option value="damaged">Some of my order arrived damaged</option>
+            <Field name="tracking" type="text" component={Input} label="Tracking number"/>
+            <Field name="issue" type="issue" element="select" children={
+                    [
+                        <option value="late">My delivery hasn't arrived</option>,
+                        <option value="wrong">The wrong item was delivered</option>,
+                        <option value="missing">Part of my order was missing</option>,
+                        <option value="damaged">Some of my order arrived damaged</option>,
                         <option value="other">Other (give details below)</option>
-                    </select>
-                );
-            }} label="What is your issue?"/>
-            <label htmlFor="details">Give more details (optional)</label>
-            <Field name="details" component="textarea" label="Give more details (optional)"/>
+                    ]
+                } component={Input} label="What is your issue?"/>
+            <Field name="details" element="textarea" component={Input} label="Give more details (optional)"/>
             <button type="submit">Submit</button>
         </form>
         );
